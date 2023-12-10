55a42abb7707d74ff1f822b644733ebf83c05acb
BuildTools <unconfigured@null.spigotmc.org>
2023-12-10 17:25:32 +0100

    Changes by Krzysiek

--- src/main/java/net/pullolo/adventcalendar/AdventCalendar.java
+++ src/main/java/net/pullolo/adventcalendar/AdventCalendar.java
@@ -1,6 +1,17 @@
 package net.pullolo.adventcalendar;
 
+import net.pullolo.adventcalendar.commands.Gui;
+import net.pullolo.adventcalendar.data.DbManager;
+import net.pullolo.adventcalendar.data.PlayerData;
+import net.pullolo.adventcalendar.events.DataEventsHandler;
+import net.pullolo.adventcalendar.gui.GuiManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+import static net.pullolo.adventcalendar.data.PlayerData.getPlayerData;
 
 public final class AdventCalendar extends JavaPlugin {
 
@@ -11,7 +22,17 @@ public final class AdventCalendar extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        config = getConfig();
+        saveDefaultConfig();
+        dbManager = new DbManager(this);
+        dbManager.init();
+        checkDb();
+        setPlayerData();
+        final GuiManager gui = new GuiManager(this);
+        Gui guiCmd = new Gui(gui);
+        getServer().getPluginManager().registerEvents(new DataEventsHandler(dbManager, gui, this), this);
+        getCommand("gui").setExecutor(guiCmd);
+        getCommand("gui").setTabCompleter(guiCmd);
     }
 
     @Override
